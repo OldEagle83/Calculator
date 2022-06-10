30908ba0973839c2b2f43616dc54e5e47dca2bf8
OldEagle83 <john.geroitos@gmail.com>
2022-06-10 21:26:26 +0300

    Exercise fork, do not push/pull

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 yes = ['y', 'Y']
 no = ['n', 'N']
 mem = ['m', 'M']
@@ -46,6 +50,26 @@ def is_oper(oper):
     else:
         return [True, oper]
 
+def is_one_digit(v):
+    if 10 > v > -10 and float(v).is_integer():
+        return True
+    else:
+        return False
+
+def check(v1, v2, v3):
+    msg = ''
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg = msg + msg_6
+    if (v1 == 1 or v2 == 1) and v3 == '*':
+        msg = msg + msg_7
+    if (v1 == 0 or v2 == 0) and v3 in ['*', '+', '-']:
+        msg = msg + msg_8
+    if msg != '':
+        msg = msg_9 + msg
+        print(msg)
+
+
+
 
 while not flag:
     flag_1 = False
@@ -67,6 +91,7 @@ while not flag:
         x = x[1]
         y = y[1]
         oper = oper[1]
+        check(x, y, oper)
         flag = True
 
         if y == 0 and oper == '/':
@@ -92,5 +117,4 @@ while not flag:
                         flag_1 = True
                     elif answer_5 in no:
                         break
-
 
